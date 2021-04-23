a613e299c574f8ac351afd6fcbc7b40f579928f7
ujjwal <ujjwal24batra@gmail.com>
2021-04-23 15:04:10 +0530

    readme updated

--- server/index.js
+++ server/index.js
@@ -221,7 +221,7 @@ app.get('/eventWinners', function (req, res) {
 })
 
 // establishing backend server
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("APIs listning at http://localhost:3000/")
